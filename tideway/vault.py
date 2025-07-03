39e39b075b9e455ac84e92e3e8509b131f7cf293
Wesley Moskal-Fitzpatrick <github@fitzmoskal.me>
2025-07-03 15:04:39 +0100

    Add deprecation wrappers for duplicated API methods (#32)

--- tideway/vault.py
+++ tideway/vault.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import tideway
+import warnings
 
 dr = tideway.discoRequests
 appliance = tideway.main.Appliance
@@ -8,11 +9,18 @@ appliance = tideway.main.Appliance
 class Vault(appliance):
     '''Manage the credential vault.'''
 
+    def get_vault(self):
+        '''Get details of the state of the vault.'''
+        return dr.discoRequest(self, "/vault")
+
     def getVault(self):
         '''Get details of the state of the vault.'''
-        response = dr.discoRequest(self, "/vault")
-        return response
-    get_vault = property(getVault)
+        warnings.warn(
+            "getVault() is deprecated; use get_vault() instead.",
+            DeprecationWarning,
+        )
+        return self.get_vault()
+    get_vault_property = property(get_vault)
 
     def patch_vault(self, body):
         '''Alternate API call for PATCH /vault'''
@@ -21,5 +29,8 @@ class Vault(appliance):
 
     def updateVault(self, body):
         '''Change the state of the vault.'''
-        response = dr.discoPatch(self, "/vault", body)
-        return response
+        warnings.warn(
+            "updateVault() is deprecated; use patch_vault() instead.",
+            DeprecationWarning,
+        )
+        return self.patch_vault(body)
